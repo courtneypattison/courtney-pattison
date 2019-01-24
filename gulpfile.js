8ec5d222bf313fdce548dff0f33a50e4e7881579
courtneypattison <courtney@courtneypattison.com>
2019-01-24 13:51:37 -0800

    Changed gulp image plugins

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var cssnano = require('gulp-cssnano');
 var gm = require('gulp-gm');
 var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
+var jp2 = require('gulp-jpeg-2000');
+var jxr = require('gulp-jpeg-xr');
 var rename = require("gulp-rename");
+var webp = require("gulp-webp");
 
 var paths = {
   css: {
@@ -24,45 +27,46 @@ function html() {
   return gulp.src(paths.html.src)
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(paths.html.dest));
-};
+}
 
 function css() {
   return gulp.src(paths.css.src)
     .pipe(cssnano())
     .pipe(gulp.dest(paths.css.dest));
-};
+}
 
 function imagesFormatted(size) {
   return gulp.src(paths.images.src)
-    .pipe(gm(function (file) { return file.resize(size, size); }, { imageMagick: true }))
+    .pipe(gm(function (file) { return file.resize(size, size); }))
     .pipe(rename(function (path) { path.basename = `${path.basename}@${size}w`; }));
 }
 
 function imagesWebp(size) {
   imagesFormatted(size)
-    .pipe(gm(function (file) { return file.setFormat('webp'); }, { imageMagick: true }))
-    .pipe(gulp.dest(paths.images.dest))
+    .pipe(webp())
+    .pipe(gulp.dest(paths.images.dest));
 }
 
+// Requires nConvert
 function imagesJxr(size) {
   imagesFormatted(size)
-    .pipe(gm(function (file) { return file.setFormat('jxr'); }, { imageMagick: true }))
-    .pipe(gulp.dest(paths.images.dest))
+    .pipe(jxr())
+    .pipe(gulp.dest(paths.images.dest));
 }
 
+// Requires ImageMagick
 function imagesJp2(size) {
   imagesFormatted(size)
-    .pipe(gm(function (file) { return file.setFormat('jp2'); }, { imageMagick: true }))
-    .pipe(gulp.dest(paths.images.dest))
+    .pipe(jp2())
+    .pipe(gulp.dest(paths.images.dest));
 }
 
 function imagesPngJpeg(size) {
   imagesFormatted(size)
     .pipe(imagemin())
-    .pipe(gulp.dest(paths.images.dest))
+    .pipe(gulp.dest(paths.images.dest));
 }
 
-// Requires imagemagick and jxrlib
 function images() {
   return new Promise(function (resolve) {
     [320, 480, 640, 960, 1280].forEach(function (size) {
