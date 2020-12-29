e606c846bbc7c9485e8ede80cf3f78bd33a53861
courtneypattison <courtneyp@ttison.ca>
2020-12-29 17:08:17 -0500

    Added base content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,148 +1,138 @@
 import * as React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 // styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+const pageStyle = {
+  alignItems: "center",
+  backgroundColor: "#ffe082",
+  display: "flex",
+  flexDirection: "column",
+  padding: "1em",
+
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+
+const profileImageStyle = {
+  borderRadius: "50%",
+  border: "1px solid black",
 }
-const headingAccentStyles = {
-  color: "#663399",
+
+const projectStyle = {
+  maxWidth: "320px",
+  margin: "1rem",
 }
-const paragraphStyles = {
-  marginBottom: 48,
+
+const projectsWrapperStyle = {
+  display: "flex",
+  flexFlow: "row wrap",
 }
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
+
+const socialLinkStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  
 }
 
 const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
+  color: "black",
+  textDecoration: "none",
+  borderRadius: "25%",
 }
 
 const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+const tagStyle = {
 }
+
 // data
-const links = [
+const projects = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    name: "gulp-jpeg-2000",
+    description: "A gulp plugin for converting images to JPEG 2000 (JP2) using ImageMagick with over 5K downloads.",
+    year: 2018,
+    url: "https://www.npmjs.com/package/gulp-jpeg-2000",
+    source: "https://github.com/courtneypattison/gulp-jpeg-2000",
+    img: "gulp-jpeg-2000.png",
+    tags: ["Javascript", "Gulp", "Mocha", "Travis CI", "Coveralls"]
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    name: "gulp-jpeg-xr",
+    description: "A gulp plugin for converting images to JPEG XR (JXR) using nConvert with over 2K downloads.",
+    year: 2018,
+    url: "https://www.npmjs.com/package/gulp-jpeg-xr",
+    source: "https://github.com/courtneypattison/gulp-jpeg-xr",
+    img: "gulp-jpeg-xr.png",
+    tags: ["Javascript", "Gulp", "Mocha", "Travis CI", "Coveralls"]
   },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
+];
+
+// helper functions
+function getFixedImage(data, originalName) {
+  return data.allFile.edges.find(edge => edge.node.childImageSharp.fixed.originalName === originalName).node.childImageSharp.fixed
+}
+
+function getTags(tags) {
+  return tags.map(tag => (
+    <span style={tagStyle}>{tag} </span>
+  ));
+}
+
+function getProjects(images) {
+  return (
+    <div style={projectsWrapperStyle}>
+      {projects.map(project => (
+        <article style={projectStyle}>
+          <h2><a style={linkStyle} href={project.url}>{project.name}</a></h2>
+          <p>{project.year}</p>
+          <p style={descriptionStyle}>{project.description}</p>
+          {getTags(project.tags)}
+          <a style={linkStyle} href={project.source}>Github</a>
+          <Img fixed={getFixedImage(images, project.img)} alt="" />
+        </article>
+      ))}
+    </div>
+  );
+}
+
+function getHeader(images) {
+  return (
+    <header>
+      <Img style={profileImageStyle} fixed={getFixedImage(images, "courtney.png")} alt=""/>
+
+      <div style={socialLinkStyle}>
+        <a style={linkStyle} href="https://github.com/courtneypattison">GitHub</a>
+        <a style={linkStyle} href="https://www.linkedin.com/in/courtney-pattison/">LinkedIn</a>
+        <a style={linkStyle} href="https://stackoverflow.com/users/3376039/courtney-pattison">Stack Overflow</a>
+      </div>
+    </header>
+  );
+}
 
 // markup
 const IndexPage = () => {
+  const images = useStaticQuery(graphql`
+  query MyQuery {
+    allFile(filter: {relativeDirectory: {eq: "images"}}) {
+      edges {
+        node {
+          childImageSharp {
+            fixed(width: 320, height: 320) {
+              originalName
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+  `);
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+    <main style={pageStyle}>
+      <title>Courtney Pattison</title>
+      {getHeader(images)}
+      {getProjects(images)}
     </main>
   )
 }
