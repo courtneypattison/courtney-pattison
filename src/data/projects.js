b4d59c7653825cd4996159701042d33aa5807dff
courtneypattison <courtneyp@ttison.ca>
2020-12-30 14:43:05 -0500

    Moved projects to own file

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    name: "gulp-jpeg-2000",
+    description:
+      "A gulp plugin for converting images to JPEG 2000 (JP2) using ImageMagick with over 5K downloads.",
+    year: 2018,
+    url: "https://www.npmjs.com/package/gulp-jpeg-2000",
+    source: "https://github.com/courtneypattison/gulp-jpeg-2000",
+    img: "gulp-jpeg-2000.png",
+    tags: ["Javascript", "Gulp", "Mocha", "Travis CI", "Coveralls"],
+  },
+  {
+    name: "gulp-jpeg-xr",
+    description:
+      "A gulp plugin for converting images to JPEG XR (JXR) using nConvert with over 2K downloads.",
+    year: 2018,
+    url: "https://www.npmjs.com/package/gulp-jpeg-xr",
+    source: "https://github.com/courtneypattison/gulp-jpeg-xr",
+    img: "gulp-jpeg-xr.png",
+    tags: ["Javascript", "Gulp", "Mocha", "Travis CI", "Coveralls"],
+  },
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import { graphql, useStaticQuery } from "gatsby";
 import Img from "gatsby-image";
 
 import Header from "../components/header";
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+
+import { projects } from "../data/projects";
 
 // styles
 const Project = styled.article`
@@ -26,30 +28,6 @@ const Link = styled.a`
 const Description = styled.p``;
 
 const Tag = styled.span``;
-
-// data
-const projects = [
-  {
-    name: "gulp-jpeg-2000",
-    description:
-      "A gulp plugin for converting images to JPEG 2000 (JP2) using ImageMagick with over 5K downloads.",
-    year: 2018,
-    url: "https://www.npmjs.com/package/gulp-jpeg-2000",
-    source: "https://github.com/courtneypattison/gulp-jpeg-2000",
-    img: "gulp-jpeg-2000.png",
-    tags: ["Javascript", "Gulp", "Mocha", "Travis CI", "Coveralls"],
-  },
-  {
-    name: "gulp-jpeg-xr",
-    description:
-      "A gulp plugin for converting images to JPEG XR (JXR) using nConvert with over 2K downloads.",
-    year: 2018,
-    url: "https://www.npmjs.com/package/gulp-jpeg-xr",
-    source: "https://github.com/courtneypattison/gulp-jpeg-xr",
-    img: "gulp-jpeg-xr.png",
-    tags: ["Javascript", "Gulp", "Mocha", "Travis CI", "Coveralls"],
-  },
-];
 
 // helper functions
 function getFixedImage(data, originalName) {
