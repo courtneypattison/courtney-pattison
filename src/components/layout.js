6bf423558413dc930f2a7aca2135fdcf1e4e374d
courtneypattison <courtneyp@ttison.ca>
2021-01-03 13:29:01 -0500

    Removed background colour

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,6 @@ import styled from "@emotion/styled"
 
 const Wrapper = styled.div`
   align-items: center;
-  background-color: #ffe082;
   display: flex;
   flex-direction: column;
   padding: 1em;
