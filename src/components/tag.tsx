61dfcd572e7456dad5376dea6cc91a34cc5e3fdd
courtneypattison <courtneyp@ttison.ca>
2021-01-06 21:27:56 -0500

    Styled tags

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -2,10 +2,23 @@ import React, { ReactElement } from "react";
 import styled from "@emotion/styled";
 
 const TagWrapper = styled.button`
+  background-color: #e0e0e0;
+  border-radius: 20px;
+  border-style: none;
+  font-size: 14px;
   margin-right: 6px;
   margin-bottom: 6px;
-  border-radius: 20px;
-  font-size: 14px;
+
+  &:hover {
+    background-color: #d8d8d8;
+  }
+  &:active {
+    background-color: #e0e0e0;
+  }
+  &:focus {
+    background-color: #bbbbbb;
+    outline: none;
+  }
 `;
 
 export interface TagI {
@@ -20,17 +33,25 @@ interface TagProps {
 }
 
 const Tag = ({ handleTagClick, isSelected, name }: TagProps): ReactElement => {
-  const handleSelected = () => {
-    return isSelected ? " \u2713" : "";
+  const removeCount = (name: string): string => {
+    return name.replace(/\s\(.+\)/g, "");
   };
 
-  const removeCount = (name: string): string => {
-    return name.replace(/\s\(.+\)/g, '');
-  };
+  if (isSelected) {
+    return (
+      <TagWrapper
+        onClick={handleTagClick}
+        style={{ backgroundColor: "#C4C4C4" }}
+        value={removeCount(name)}
+      >
+        {"\u2713 "}
+        {name}
+      </TagWrapper>
+    );
+  }
 
   return (
     <TagWrapper onClick={handleTagClick} value={removeCount(name)}>
-      {handleSelected()}
       {name}
     </TagWrapper>
   );
