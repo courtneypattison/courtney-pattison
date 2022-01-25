a9563965197d6452cb91c0fe9a11fe6866d84034
courtneypattison <courtneyp@ttison.ca>
2022-01-25 15:26:11 -0500

    Removed analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from "react";
-import firebase from "gatsby-plugin-firebase";
 import { graphql } from "gatsby";
 
 import Header from "../components/header";
@@ -7,10 +6,6 @@ import Layout from "../components/layout";
 import ProjectList from "../components/project-list";
 
 const IndexPage = ({ data }: any): ReactElement => {
-  React.useEffect(() => {
-    if (!firebase) return;
-    firebase.analytics().logEvent("visited_index");
-  }, []);
   return (
     <Layout>
       <title>{data.site.siteMetadata.title}</title>
